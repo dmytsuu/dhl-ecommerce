e1eaddc14164f96e72f32c0a8c56cb1d9087b7d7
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-01-15 17:04:34 +0200

    Remove unnessecary files

--- spec/ecommerce.rb
+++ spec/ecommerce.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 RSpec.describe DhlEcommerce do
-  it "has a version number" do
+  it 'has a version number' do
     expect(DhlEcommerce::VERSION).not_to be nil
   end
 end
